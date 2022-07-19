68cc13a72bb90a973694d85ccfb8ee3050f2a3da
Bryan Guevara <bryan19052000@gmail.com>
2022-07-18 18:46:03 -0700

    Add several components and file structure for the project

---
+++ src/components/form/FormContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/FormContainer.css';
+
+class FormContainer extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <div className='form-container'>
+
+        </div>
+    }
+}
+
+export default FormContainer;
---
+++ src/components/preview/PreviewContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/PreviewContainer.css';
+
+class PreviewContainer extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <div>
+
+        </div>
+    }
+}
+
+export default PreviewContainer;
